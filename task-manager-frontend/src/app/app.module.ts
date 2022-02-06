ce75b051f3d8336ca9ebc05985aecf20ee42539a
Niv Stibel <Nivsvb@gmail.com>
2022-02-06 17:01:44 +0200

    Created home component for displaying tasks

--- task-manager-frontend/src/app/app.module.ts
+++ task-manager-frontend/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
