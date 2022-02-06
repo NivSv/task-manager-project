723eed47687adcca1edac5aed0ba942168cc0a71
Niv Stibel <Nivsvb@gmail.com>
2022-02-06 19:00:08 +0200

    Added a mobile version to the task table

--- task-manager-frontend/src/app/app.module.ts
+++ task-manager-frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- task-manager-frontend/src/app/components/home/home.component.ts
+++ task-manager-frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+  list= []
 
   ngOnInit(): void {
+    this.http.get('https://localhost:7259/api/WowcharactersTable').subscribe(data => console.log(data));
   }
 
 }
